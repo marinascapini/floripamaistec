6aa3a8a77e9bc9fabf4c0bba808942682a872981
marinascapini <124382759+marinascapini@users.noreply.github.com>
2023-02-16 16:03:37 -0300

    Add files via upload

---
+++ Módulo 01/Semana 03/exercicio_01/Exercicio 01 semana 03/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Exercicio_01_semana_03
+{
+	public class Produto
+	{
+		public string nome;
+		public double preco;
+		public int quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
+        public Produto()
+        {
+            nome = "Sem nome";
+            preco = 0;
+            quantidade = 0;
+        }
+    }
+}
---
+++ Módulo 01/Semana 03/exercicio_01/Exercicio 01 semana 03/Program.cs
@@ -0,0 +1,13 @@
+﻿using Exercicio_01_semana_03;
+
+// após definir as classes:
+
+Produto produto1 = new Produto("Calculadora", 5.75, 199);
+
+Produto produto2 = new Produto();
+
+// para imprimir no terminal:
+
+Console.WriteLine($"O produto1 tem nome: {produto1.nome}, valor: {produto1.preco}, quantidade: {produto1.quantidade}");
+
+Console.WriteLine($"O produto2 tem nome: {produto2.nome}, valor: {produto2.preco}, quantidade: {produto2.quantidade}");
