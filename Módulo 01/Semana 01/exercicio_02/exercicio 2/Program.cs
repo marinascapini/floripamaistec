5b62254b0c9053e27e486f357982b4d466a240de
marinascapini <124382759+marinascapini@users.noreply.github.com>
2023-02-16 20:02:29 -0300

    Add files via upload

---
+++ Módulo 01/Semana 01/exercicio_02/exercicio 2/Program.cs
@@ -0,0 +1,28 @@
+﻿List<string> frutas = new List<string>();
+bool continuar = true;  // pois é o usuário que irá inserir as frutas
+
+while (continuar)
+{
+    if (frutas.Count() == 15) // máximo de 15 itens
+    {
+        continuar = false;
+        break;
+    }
+
+    Console.Write("Digite o nome de uma fruta: ");
+    frutas.Add(Console.ReadLine()); // read line necessita que o usuário digite enter para ler, é preferível
+    Console.Write("Você deseja continuar? ");
+    string resposta = Console.ReadLine();
+
+    if (resposta == "Nao" || resposta == "n")
+    {
+        continuar = false;
+    }
+}
+
+frutas.Sort(); // colocar as frutas em ordem alfabética
+
+foreach (string fruta in frutas)
+{
+    Console.WriteLine(fruta);
+}
