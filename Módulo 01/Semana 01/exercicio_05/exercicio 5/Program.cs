489864d27a2433edaed128eb81a617ee9514e36d
marinascapini <124382759+marinascapini@users.noreply.github.com>
2023-02-16 20:06:00 -0300

    Add files via upload

---
+++ Módulo 01/Semana 01/exercicio_05/exercicio 5/Program.cs
@@ -0,0 +1,15 @@
+﻿// sequencia de fibonacci: 0 1 1 2 3 5 8 13 - 0 + 1 = 1 + 2 = 3, etc...
+// se quiser números muito grandes, no lugar do int usar o long, e se quiser número gigante, usar big int
+
+int n;
+int x = 1, y = 0, z = 0;
+n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    // z = 0, x = 1, y = 0.
+    z = x + y; // soma o anterior com o atual
+    Console.WriteLine(z);
+    y = x;
+    x = z;
+}
